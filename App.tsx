dd0f2a2d8eaab8bd8b391f6a8a0e851cd543784c
RhayPradoF <rhayssa.c.prado@gmail.com>
2024-11-19 20:15:06 -0300

    Inclusão de pastas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import {Text, View } from 'react-native';
+import ChatScreen from "./src/screens/ChatScreen";
 
 export default function App() {
   return (
     <View >
       <StatusBar style="auto" />
-      <Text>Inicio da SOBRA MODA</Text>
+      <ChatScreen />
       
     </View>
   );
