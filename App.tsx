f898316121814af65d16dcc67a7280ef7bb0198e
RhayPradoF <rhayssa.c.prado@gmail.com>
2024-11-20 10:06:37 -0300

    Inclusão de rotas e navegação em pilha

--- App.tsx
+++ App.tsx
@@ -3,22 +3,27 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import { Chat } from "./src/screens/Chat";
 import { PrivateChat } from "./src/screens/PrivateChat";
+import { NoImplements } from "./src/screens/NoImplements";
+import { Navigator } from "./src/navigation";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Chat"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Chat" component={Chat} />
-        {/* <Stack.Screen name="PrivateChat" component={PrivateChat}/> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator
+    //     initialRouteName="Chat"
+    //     screenOptions={{
+    //       headerShown: false,
+    //     }}
+    //   >
+    //     <Stack.Screen name="Chat" component={Chat} />
+    //     {/* <Stack.Screen name="PrivateChat" component={PrivateChat}/> */}
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+    // <NoImplements/>
+    <Navigator/>
 
   );
 }
---
+++ src/navigation/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import {colors} from "../styles/colors";
+import { Chat } from "../screens/Chat";
+import { MainTabNavigator } from "./MainTabNavigation";
+
+
+
+const Stack = createNativeStackNavigator();
+
+export const Navigator = ()=>{
+    return(
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Home" //depois que terminar colocar a Login para ser a principal
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: colors.purple.cor5,
+                    },
+                }}
+            >
+                <Stack.Screen
+                    name="Home"
+                    component={MainTabNavigator}
+                    options={{
+                        title: "Bem-vindo",
+                      /*   headerShadowVisible: false, */
+                        headerShown: false,
+                        /* fullScreenGestureShadowEnabled: false, */
+
+                    }}
+
+                />
+
+                {/* <Stack.Screen
+                    name="Home"
+                    component={MainTabNavigator}
+                    options={{ headerShown: false }}
+                />
+
+                <Stack.Screen
+                    name="Chat"
+                    component={ChatScreen}
+                    options={({navigation}) => ({
+                        headerTitle:"Mensagem",
+                        headerTitleAlign: "center",
+                    })}
+
+                />
+
+                <Stack.Screen
+                    name="Contacts"
+                    component={ContatoScreen}
+                    options={{ headerTitle: "Contatos",
+                        headerTitleAlign: "center",
+                    }}
+                />
+
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                    options={{ title: "Login", headerShown: false }}
+                /> */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
