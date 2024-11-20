6f96037164a6d617c24a566d4a6bee1758c2b4a3
TatianePink <tpink@faeterj-petropolis.edu.br>
2024-11-20 01:36:16 -0300

    home

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import {Text, View } from 'react-native';
+import Home from './src/screens/Home';
 
 export default function App() {
   return (
     <View >
       <StatusBar style="auto" />
-      <Text>Inicio da SOBRA MODA</Text>
+      <Home />
+      {/* <Text>Inicio da SOBRA MODA</Text> */}
       
     </View>
   );
