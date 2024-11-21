91e8a4a06e43e6d3f5bcd15dc20b16c11fcbe568
TatianePink <tpink@faeterj-petropolis.edu.br>
2024-11-21 11:46:57 -0300

    Add

--- App.tsx
+++ App.tsx
@@ -4,10 +4,22 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import { Chat } from "./src/screens/Chat";
 import { PrivateChat } from "./src/screens/PrivateChat";
+import  Add  from "./src/screens/Add";
+import { useFonts,ComicNeue_400Regular,
+  ComicNeue_700Bold } from "@expo-google-fonts/comic-neue";
+  import AppLoading from "expo-app-loading";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+
+// const[FontsLoaded]= useFonts({ComicNeue_400Regular,
+//   ComicNeue_700Bold })
+
+//   if(!FontsLoaded)
+//     <AppLoading />
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -22,4 +34,6 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+
 
